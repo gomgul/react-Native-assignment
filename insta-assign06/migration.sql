70339bd4fb80b9597e7de3b85a222dc22a9cc3ae
gom <nobrain908@gmail.com>
2021-12-02 22:05:15 +0900

    s1

---
+++ insta-assign06/migration.sql
@@ -0,0 +1,5 @@
+ALTER TABLE "User" ADD COLUMN     "avatarURL" TEXT,
+ADD COLUMN     "githubUsername" TEXT,
+ADD COLUMN     "location" TEXT,
+ADD COLUMN     "name" TEXT,
+ADD COLUMN     "password" TEXT NOT NULL;
---
+++ insta-assign06/users.typeDefs.ts
@@ -0,0 +1,17 @@
+import { gql } from "apollo-server";
+export default gql`
+  type User {
+    id: Int!
+    username: String!
+    email: String!
+    name: String
+    avatarURL: String
+    location: String
+    githubUsername: String
+    createdAt: String
+    updatedAt: String
+  }
+  type Query {
+    seeUser(id: Int!): User
+  }
+`;
